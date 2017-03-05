6ab665f2bc1d14f2782351a6d855c42deef5d5e2
May Ben Arie <may.benarie@vonage.com>
2017-03-05 23:57:54 +0200

    add flower to networking

--- networking_may.py
+++ networking_may.py
@@ -13,7 +13,8 @@ def send(cycle_number,
          small_sheep0_data,
          big_sheep12_data,
          big_sheep34_data,
-         signs):
+         signs,
+         flower_data):
 
         header = array.array('B', [0, (cycle_number / (256 * 256) ) % 256, (cycle_number / 256) % 256, cycle_number % 256])
         pixels_data = array.array('B', big_sheep12_data[0:900])
@@ -39,6 +40,10 @@ def send(cycle_number,
         pixels_data = array.array('B', small_sheep0_data)
         message_s5 = (header + pixels_data).tostring()
 
+        header = array.array('B', [6, (cycle_number / (256 * 256) ) % 256, (cycle_number / 256) % 256, cycle_number % 256])
+        pixels_data = array.array('B', flower_data)
+        message_s6 = (header + pixels_data).tostring()
+
 
         sock.sendto(message_s0, (CONTROLER_IP, UDP_PORT))
         sock.sendto(message_s1, (CONTROLER_IP, UDP_PORT))
@@ -46,6 +51,7 @@ def send(cycle_number,
         sock.sendto(message_s3, (CONTROLER_IP, UDP_PORT))
         sock.sendto(message_s4, (CONTROLER_IP, UDP_PORT))
         sock.sendto(message_s5, (CONTROLER_IP, UDP_PORT))
+        sock.sendto(message_s6, (CONTROLER_IP, UDP_PORT))
 
 
 def sendSigns(cycle_number, data):
