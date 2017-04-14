4d94850d3afce52fe7adff63b0b325fc79634afc
Mickey Iluz <mixi.il@gmail.com>
2017-04-14 17:13:45 +0300

    modify mapping test

---
+++ Models/import_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(__file__)
+sys.path.append('%s/../Network' % SCRIPT_DIR)
+
+import lbproto
--- Network/test_strip.py
+++ Network/test_strip.py
@@ -6,12 +6,13 @@ import ipdb
 def test_strips(lbp):
   led  = 0
   strip = 3
+  pixel_id = 0
   while True:
-    s = raw_input('[strip %d, led %3d]:' % (strip, led))
+    s = raw_input('[strip %d, led %3d, pixel_id %d]:' % (strip, led, pixel_id))
 
     if not s:
       led += 1
-      if led >= 600:
+      if led >= 300:
         strip += 1
         led = 0
 
@@ -21,13 +22,16 @@ def test_strips(lbp):
       if s[1] == 's':
         strip = int(s[2])
         led = 0
+      if s[1] == 'p':
+        pixel_id = int(s.split(' ')[1])
+        led = 0
   
-    leds = [0] * 150 * 3
+    leds = [0] * 300 * 3
     leds[3*led + 0] = 255
     leds[3*led + 1] = 255
     leds[3*led + 2] = 255
 
-    lbp.send_simple(strip, leds, debug=False)
+    lbp.send_simple(strip, leds, pixel_id=pixel_id, debug=False)
 
 
 
