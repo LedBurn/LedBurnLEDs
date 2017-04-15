9028b268c0f3424baf5a0b2916650339f3374aa8
Mickey Iluz <mixi.il@gmail.com>
2017-04-15 18:29:46 +0300

    fix frameid issue. segments still don't work.

--- Network/lbproto.py
+++ Network/lbproto.py
@@ -1,6 +1,8 @@
 import socket
 import array
 import struct
+import math
+import ipdb
 
 class LBProtocol(object):
   
@@ -10,6 +12,9 @@ class LBProtocol(object):
   # protocol 8-bytes header
   HEADER = 'LedBurn\x00'
 
+  # packet split size by leds (note each led is 3 bytes)
+  SEGMENT_SIZE = 300 * 3
+
   def __init__(self, address):
     self.sock = socket.socket(socket.AF_INET, # Internet
                               socket.SOCK_DGRAM) # UDP
@@ -17,18 +22,20 @@ class LBProtocol(object):
     self.address = address
   
   
-  def send_simple(self, strip_id, rgb_array, pixel_id=0, **kwargs):
+  def send_simple(self, strip_id, rgb_array, pixel_id=0, segments=1, segment_id=0, **kwargs):
 
     # protocol header
     msg = self.HEADER 
     
+    if segments == 1 or segments > 1 and segment_id == 0:
+      self.frameid += 1200
+
     # header details
-    msg += struct.pack('>I', self.frameid)
-    self.frameid += 1200
+    msg += struct.pack('<I', self.frameid)
 
     # simple send, only 1 segment
-    msg += struct.pack('<I', 1)
-    msg += struct.pack('<I', 0)
+    msg += struct.pack('<I', segments)
+    msg += struct.pack('<I', segment_id)
 
     # strip address and pixel offset
     msg += struct.pack('<H', strip_id)
@@ -38,7 +45,24 @@ class LBProtocol(object):
     msg += array.array('B', rgb_array).tostring()
 
     if 'debug' in kwargs and kwargs['debug']:
+      print "frame_id: %d, segments: %d, segment_id: %d, strip_id: %d, pixel_id: %d" % \
+            (self.frameid, segments, segment_id, strip_id, pixel_id)
       print ":".join("{:02x}".format(ord(c)) for c in msg)
 
     self.sock.sendto(msg, (self.address, self.UDP_PORT))
     
+
+  def send(self, strip_id, rgb_array, **kwargs):
+    
+    # calculate number of packets (send_simple commands)
+    segments = int(math.ceil(float(len(rgb_array)) / self.SEGMENT_SIZE))
+
+    for segment_id in xrange(segments):
+      # calculate start and end of segments
+      start_of_segment = segment_id * self.SEGMENT_SIZE 
+      end_of_segment = min(start_of_segment + self.SEGMENT_SIZE, len(rgb_array))
+
+      # send sub array of pixels
+      seg_array = rgb_array[start_of_segment:end_of_segment]
+      self.send_simple(strip_id, seg_array, segments=segments, segment_id=segment_id, **kwargs)
+
