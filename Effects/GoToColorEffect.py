3ebd67d67b56f3676b93137e0df6c7d946cb10af
May Ben Arie <May22989@gmail.com>
2017-04-15 11:39:14 +0300

    Go to effect

---
+++ Effects/GoToColorEffect.py
@@ -0,0 +1,37 @@
+import math
+from AbstractEffect import Effect
+
+import sys, os
+sys.path.append(os.path.abspath('../Colors'))
+import Colors
+
+class GoToColorEffectType:
+    CONST_SPEED = 0
+    FAST_TO_SLOW = 1
+    SLOW_TO_FAST = 2
+
+class GoToColorEffect(Effect):
+    def __init__(self, indexes, from_color, to_color, type):
+        Effect.__init__(self, indexes)
+        self.from_color = from_color
+        self.to_color = to_color
+        self.type = type
+            
+    
+    def apply(self, time_precent, parent_array):
+        
+        power = time_precent
+        if self.type == GoToColorEffectType.FAST_TO_SLOW:
+            power = math.pow(time_precent, 0.25)
+        elif self.type == GoToColorEffectType.SLOW_TO_FAST:
+            power = math.pow(time_precent, 4)
+
+
+        color = Colors.go_to_color(self.from_color, self.to_color, power)
+
+        for i in self.indexes:
+            parent_array[i*3 : i*3+3] = color
+
+    
+
+
