3dd0d82855e59426b792e8862a3730f145be4637
Amir Blum <amirgiraffe@gmail.com>
2017-04-21 18:28:44 +0300

    New sheep spikes animation

--- Effects/SpikeEffect.py
+++ Effects/SpikeEffect.py
@@ -1,8 +1,9 @@
 from AbstractEffect import Effect
 
+from Colors import Colors
 
 class SpikeEffect(Effect):
-    def __init__(self, indexes, base_color, tail_percent, tot_range_pixels):
+    def __init__(self, indexes, timed_color, tail_percent, tot_range_pixels):
         """
         :param indexes: 
         :param base_color: 
@@ -10,7 +11,7 @@ class SpikeEffect(Effect):
         :param tot_range_pixels: you can decide that the total number of pixel will be different than len(indexes)
         """
         Effect.__init__(self, indexes)
-        self.base_color = base_color
+        self.timed_color = timed_color
         self.tail_percent = tail_percent
         self.tot_range_pixels = tot_range_pixels
         self.tot_pixel_with_tail = (1 + tail_percent) * tot_range_pixels
@@ -23,7 +24,7 @@ class SpikeEffect(Effect):
             color = [0, 0, 0]
             if i >= bottom_pixel and i <= top_pixel:
                 i_percent = float(i - bottom_pixel) / float(top_pixel - bottom_pixel)
-                color = [int(c * i_percent) for c in self.base_color]
+                color = Colors.change_rgb_lightness(self.timed_color.get_color(time_precent, None), Colors.fix_lightness_percent(i_percent))
             index_in_obj = self.indexes[i]
             parent_array[index_in_obj * 3: index_in_obj * 3 + 3] = color
 
